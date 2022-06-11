a866943f4f12d24950a70e89cc972537b8667925
assebc <claudioafbessa@gmail.com>
2022-06-11 10:51:20 +0100

    lets do this

---
+++ src/main.dart
@@ -0,0 +1,9 @@
+import 'acc_creation.dart';
+import 'admin.dart';
+import 'contact.dart';
+import 'cronometer.dart';
+import 'funcs.dart';
+import 'mapa.dart';
+import 'opening.dart';
+
+void main() {}
