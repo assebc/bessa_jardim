2d33d581f6a02df13cec787962c916af15a928ca
assebc <claudioafbessa@gmail.com>
2022-06-12 12:32:15 +0100

    lets go slow

--- android/app/src/main/kotlin/com/example/bessa_jardim/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.bessa_jardim
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- src/main.dart
+++
@@ -1,9 +0,0 @@
-import 'acc_creation.dart';
-import 'admin.dart';
-import 'contact.dart';
-import 'cronometer.dart';
-import 'funcs.dart';
-import 'mapa.dart';
-import 'opening.dart';
-
-void main() {}
